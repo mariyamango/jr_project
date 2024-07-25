496694d3478aedc2d3421a20ada315798f41433a
mariyamango <de.msomkova@gmail.com>
2024-07-25 18:11:30 +0200

    The images comparing added

---
+++ src/main/java/com/github/mariyamango/mavendemo/ImageComparator.java
@@ -0,0 +1,32 @@
+package com.github.mariyamango.mavendemo;
+
+
+import com.github.romankh3.image.comparison.ImageComparison;
+import com.github.romankh3.image.comparison.model.ImageComparisonResult;
+import com.github.romankh3.image.comparison.model.ImageComparisonState;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Class created for answering, if two images are the same or not.
+ */
+public class ImageComparator {
+
+    /**
+     * Method, which says if two images are the same or not.
+     *
+     * @param image1 image1 for comparison
+     * @param image2 image2 for comparison
+     * @return return true, if images are the same, false - otherwise.
+     */
+    public boolean isTheSameImage(BufferedImage image1, BufferedImage image2) {
+        //Create ImageComparison object
+        ImageComparison imageComparison = new ImageComparison(image1, image2);
+
+        //Compare images
+        ImageComparisonResult imageComparisonResult = imageComparison.compareImages();
+
+        //Check, that ImageComparisonState is MATCH:
+        return ImageComparisonState.MATCH == imageComparisonResult.getImageComparisonState();
+    }
+}
